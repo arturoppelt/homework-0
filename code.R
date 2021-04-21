ba6c62882b3388d7eb6e3abe42504b3e66ba07ee
artur oppelt <artur.t.oppelt@gmail.com>
2021-04-21 17:28:28 -0300

    exercises update

--- code.R
+++ code.R
@@ -1,9 +1,24 @@
 library(dslabs)
 library(lubridate)
 options(digits = 3)    # 3 significant digits
+#question 3
+
+data("brexit_polls")
+sum(month(brexit_polls$startdate) == 4)
+sum(round_date(brexit_polls$enddate, unit = "week") == "2016-06-12")
+
+#question 4
+weekdays(brexit_polls$enddate)
+
+sum(weekdays(brexit_polls$enddate) == "Sunday")
+sum(weekdays(brexit_polls$enddate) == "Monday")
+sum(weekdays(brexit_polls$enddate) == "Tuesday")
+sum(weekdays(brexit_polls$enddate) == "Wednesday")
+sum(weekdays(brexit_polls$enddate) == "Thursday")
+sum(weekdays(brexit_polls$enddate) == "Friday")
+sum(weekdays(brexit_polls$enddate) == "Saturday")
 
 
-
-
-
+#question 5
+data(moviens)
 
